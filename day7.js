6e69dccce4c8aaf879c6c0a12456797a030450ee
TseIvan <itse@ualberta.ca>
2020-01-23 16:42:08 -0700

    day7 p2

--- day7.js
+++ day7.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-var res = fs.readFileSync("day7_test.txt", 'utf8').trim().split('\n');
+var res = fs.readFileSync("day7.txt", 'utf8').trim().split('\n');
 
 // { x: 123, y: 456, d: 72, e: 507, f: 492, g: 114, h: 65412, i: 65079 }
 
@@ -37,41 +37,53 @@ res.forEach((item, i) => {
   queue.push(item)
   let [first,second,third]=item.match(/[(\d)||(a-z)]{1,9}/g)
 });
-console.log(count);
+let part_b_queue = Object.assign([], queue);
+
 // A gate provides no signal until all of its inputs have a signal.
-while(queue.length){
-    item = queue.shift()
-    let [first,second,third]=item.match(/[(\d)||(a-z)]{1,9}/g)
-    if (item.includes('LSHIFT')){
-      (first in wireDict) ? wireDict[third] = leftshift(wireDict[first],second) : queue.push(item);
-    }else if (item.includes('RSHIFT')){
-      (first in wireDict) ? wireDict[third] = rightshift(wireDict[first],second) : queue.push(item);
-    }else if (item.includes('OR')){
-      (first in wireDict && second in wireDict) ? wireDict[third] = bitwiseOR(wireDict[first],wireDict[second]) : queue.push(item);
-    }else if (item.includes('AND')){
-      if (first in wireDict && second in wireDict){
-        wireDict[third] = bitwiseAND(wireDict[first],wireDict[second]);
-      }else if (isInt(first) && second in wireDict){
-        wireDict[third] = bitwiseAND(parseInt(first),wireDict[second]);
+function run(queue,p2,val){
+  while(queue.length){
+      item = queue.shift()
+      let [first,second,third]=item.match(/[(\d)||(a-z)]{1,9}/g)
+      if (item.includes('LSHIFT')){
+        (first in wireDict) ? wireDict[third] = leftshift(wireDict[first],second) : queue.push(item);
+      }else if (item.includes('RSHIFT')){
+        (first in wireDict) ? wireDict[third] = rightshift(wireDict[first],second) : queue.push(item);
+      }else if (item.includes('OR')){
+        (first in wireDict && second in wireDict) ? wireDict[third] = bitwiseOR(wireDict[first],wireDict[second]) : queue.push(item);
+      }else if (item.includes('AND')){
+        if (first in wireDict && second in wireDict){
+          wireDict[third] = bitwiseAND(wireDict[first],wireDict[second]);
+        }else if (isInt(first) && second in wireDict){
+          wireDict[third] = bitwiseAND(parseInt(first),wireDict[second]);
+        }else{
+          queue.push(item);
+        }
+      }else if (item.includes('NOT')){
+        if (first in wireDict){
+          wireDict[second] = bitwiseNOT(wireDict[first]);
+        }else{
+          queue.push(item);
+        }
       }else{
-        queue.push(item);
+        if (second == 'b' && p2 == true){
+          wireDict[second] = val;
+        }
+        else if (isInt(first)){
+          wireDict[second] = parseInt(first);
+        }else if (first in wireDict){
+          wireDict[second] = wireDict[first];
+        }else{
+          queue.push(item);
+        }
       }
-    }else if (item.includes('NOT')){
-      if (first in wireDict){
-        wireDict[second] = bitwiseNOT(wireDict[first]);
-      }else{
-        queue.push(item);
-      }
-    }else{
-      console.log(item)
-      if (isInt(first)){
-        wireDict[second] = parseInt(first);
-      }else if (first in wireDict){
-        wireDict[second] = wireDict[first];
-      }else{
-        queue.push(item);
-      }
-    }
-};
-console.log(wireDict)
+  };
+}
+run(queue,false,0);
+val = wireDict['a'];
+console.log('Part 1:')
+console.log(val);
+
+wireDict = {}
+run(part_b_queue,true,val);
+console.log('Part 2:')
 console.log(wireDict['a']);
