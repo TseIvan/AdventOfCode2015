2380239cc2e47d55d665a5c76ce434a03b2b57ee
TseIvan <itse@ualberta.ca>
2020-01-23 17:57:37 -0700

    day8p1

--- day8.js
+++ day8.js
@@ -1,9 +1,7 @@
 const fs = require('fs')
-var res = fs.readFileSync("day8.txt", 'utf8').trim().split('\n');
-
-let total_number_of_characters = 0;
+var res = fs.readFileSync("day8.txt", 'utf8').trim().split('\n')
+let total = 0;
 res.forEach((item, i) => {
-  console.log(item);
-  total_number_of_characters += item.length;
-  // Clean text
+  total += item.length-1 - eval(item).length
 });
+console.log(total+1)
