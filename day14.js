7b1c55e70c63f396e1e8ff5419f83b9187f6cc2b
TseIvan <itse@ualberta.ca>
2020-02-05 15:25:20 -0700

    day14p2

--- day14.js
+++ day14.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 var res = fs.readFileSync("day14.txt", 'utf8').trim().split("\n")
-dict = {}
+const dict = {}
 res.forEach((item, i) => {
   parsed = item.split(" ")
   dict[parsed[0]] = [parsed[3],parsed[6],parsed[13]].map(x => parseInt(x))
@@ -21,3 +21,58 @@ Object.entries(dict).forEach(([key, value]) => {
     }
 });
 console.log(maxDist);
+// Need new way of handling time so that it computes one second for each reindeer and tracks everything
+
+class Reindeer {
+  constructor(speed, stamina, recovery) {
+    this.speed = speed;
+    this.stamina = stamina;
+    this.recovery = recovery;
+    this.distance = 0;
+    this.currentStamina = this.stamina;
+    this.stalled  = 0;
+    this.points = 0;
+  }
+  oneTimeInterval(){
+    // If recovering return this.distance
+    if (this.stalled){
+        this.stalled -= 1;
+        if (this.stalled == 0){
+          this.recover();
+        }
+    }else{
+      this.move();
+      if (this.currentStamina == 0){
+        this.stalled += this.recovery;
+      }
+    }
+    return this.distance;
+  }
+  recover(){
+    this.stalled = 0;
+    this.currentStamina = this.stamina;
+  }
+  move(){
+    this.distance += this.speed;
+    this.currentStamina -= 1;
+  }
+}
+
+reindeer_list = []
+Object.entries(dict).forEach((item, i) => {
+  reindeer_list.push(new Reindeer(item[1][0],item[1][1],item[1][2]))
+});
+
+
+let iter = 2503;
+while(iter){
+  let arr = reindeer_list.map(obj => obj.oneTimeInterval())
+
+  reindeer_list.map(obj =>{
+    if (obj.distance == Math.max(...arr)){
+      obj.points++;
+    }
+  })
+  --iter;
+}
+console.log(Math.max(...reindeer_list.map(x=>x.points)))
