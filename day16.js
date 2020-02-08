cf5d5d7f24495b4b9a7f112bc1f3040bfc1f71bd
TseIvan <itse@ualberta.ca>
2020-02-08 13:21:38 -0700

    day 17 + day16-setup

---
+++ day16.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+var res = fs.readFileSync("day14.txt", 'utf8').trim().split("\n")
---
+++ day17.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+from functools import reduce
+from operator import add
+def run(f1='day17.txt'):
+    with open(f1, 'r') as fp:
+        text_from_file = fp.readlines()
+    containers = list(map(lambda x:int(x),[line.strip('\n') for line in text_from_file]))
+
+    equal_to_150 = 0
+    min_container = 10**6
+    for l  in range(1,len(containers)+1):
+        for subset in combinations(containers,l):
+                if reduce(add,subset) == 150:
+                    if (l < min_container):
+                        min_container = l
+                        equal_to_150 = 1
+                    elif (l == min_container):
+                        equal_to_150 += 1
+    print(equal_to_150) # 1304 #15
+run()
