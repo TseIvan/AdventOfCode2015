a717c91aff538c1e17734c65374a6330fedd1c53
TseIvan <itse@ualberta.ca>
2020-02-17 01:26:51 -0700

    setup day21 for tomorrow

--- day21.py
+++ day21.py
@@ -1,5 +1,6 @@
-
-weapon_shop =  {
+import itertools
+# Cost Damage Armor
+weapon_shop={
 "Dagger"        : [8,     4,       0],
 "Shortsword"   : [10,     5,       0],
 "Warhammer"    : [25,     6,       0],
@@ -7,7 +8,7 @@ weapon_shop =  {
 "Greataxe"    : [ 74,     8,       0],
 }
 
-armor_shop{
+armor_shop={
     "Leather"      : [13,    0,       1],
     "Chainmail"    : [31,     0,      2],
     "Splintmail"   : [53,    0,       3],
@@ -15,13 +16,13 @@ armor_shop{
     "Platemail"   : [102,     0,       5],
 }
 
-ring_shop{
-    "Damage +1"    [25,     1,       0],
-    "Damage +2"    [50,    2,       0],
-    "Damage +3"   [100,     3,       0],
-    "Defense +1"   [20,     0,       1],
-    "Defense +2"   [40,     0,       2],
-    "Defense +3"   [80,     0,       3],
+ring_shop={
+    "Damage +1"   : [25,     1,       0],
+    "Damage +2"   : [50,    2,       0],
+    "Damage +3"   : [100,     3,       0],
+    "Defense +1"  : [20,     0,       1],
+    "Defense +2"  : [40,     0,       2],
+    "Defense +3"  : [80,     0,       3],
 }
 
 class Character:
@@ -35,7 +36,8 @@ class Character:
         return self.cost
 
     def takeDamage(self,damage):
-        self.health -= (self.armor-damage)
+        self.health -= (damage-self.armor)
+        return
 
     def fatal(self):
         return int(self.health <= 0)
@@ -48,7 +50,27 @@ def runSimulation(player,boss):
         if (player.fatal()): return False
 
 def play():
+    wep_iter = itertools.combinations(weapon_shop.keys(),1) # no dupes
+    armor_iter = itertools.combinations(armor_shop.keys(),1) # no dupes
+    ring_iter_1 = itertools.combinations(ring_shop.keys(),1) # no duplicates
+    ring_iter_2 = itertools.combinations(ring_shop.keys(),2) # need to clean duplicates
+
+    # print(*wep_iter)
+    # [print(_) for _ in list(ring_iter_2)]
+
+    # Can only be wep + ring, wep + ring 2, wep + armor + ring, wep + armor + ring2, wep + armor.
+    # Need to run combination on each *iter then columnwise summation create character then run simulation, if success append player.cost into list.
+    # Finally, run  arr.reduce( x,y => (x<y) ? (x) : (y))
+    
+
 
     return
 
 play()
+
+    # runSimluation works
+    # For example, suppose you have 8 hit points, 5 damage, and 5 armor, and that the boss has 12 hit points, 7 damage, and 2 armor:
+    # player = Character(8,5,5,0)
+    # boss = Character(12,2,7,0)
+    #
+    # print(runSimulation(player,boss))
